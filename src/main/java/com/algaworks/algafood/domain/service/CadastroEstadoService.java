b5df462df68d8f8643b384a0cf5f63689b262288
Felipe Castro <felipe_fcastro@yahoo.com.br>
2020-05-18 19:42:09 -0300

    Adicionando Spring Data JPA

--- src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -18,13 +18,13 @@ public class CadastroEstadoService {
 	
 	
 	public Estado salvar(Estado estado) {
-		return estadoRepository.salvar(estado);
+		return estadoRepository.save(estado);
 	}
 	
 	public void excluir(Long estadoId) {
 		
 		try {
-			estadoRepository.remover(estadoId);
+			estadoRepository.deleteById(estadoId);
 			
 		} catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncontradaException(
