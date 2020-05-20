bda6a91f9fe4b2d7a0367e07727233868d9373a7
Felipe Castro <felipe_fcastro@yahoo.com.br>
2020-05-20 17:26:01 -0300

    Adicionando consulta dinâmica com JPQL

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -51,7 +51,7 @@ public class TesteController {
 	@GetMapping("/restaurantes/por-nome")
 	public List<Restaurante> restaurantesPorId(String nome, Long cozinhaId) {
 
-		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+		return restauranteRepository.consultarPorNome(nome, cozinhaId);
 	}
 
 	@GetMapping("/restaurantes/primeiro-por-nome")
@@ -64,6 +64,12 @@ public class TesteController {
 	public List<Restaurante> restaurantesTop2PorNome(String nome) {
 
 		return restauranteRepository.findTop2ByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/por-nome-e-frete")
+	public List<Restaurante> restaurantesPorNomeFrete(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+
+		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
 	}
 	
 	@GetMapping("/restaurantes/count-por-cozinha")
